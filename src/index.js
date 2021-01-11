e3e83d4e54044e2c8ee70968b602c21cf3c34f71
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2021-01-11 18:21:13 +0900

    Introdection Redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-  
+import { createStore } from "redux";
+
+//state :: application에서 바뀌는 data를 말한다
+
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+let number = document.querySelector("span");
+number.innerText = 0;
+
+const PLUS = "PLUS";
+const MINUS = "MINUS";
+//store and reducer -> always together
+//reducer is a function which modifies data ex) increase or decrease count
+//it changes data and if it returns sth, it is also data of application 
+const countModifier = (count = 0, action) => {
+  // if(action.type === "PLUS") {
+  //   return count + 1;
+  // }else if(action.type === "MINUS") {
+  //   return count - 1;
+  // }else {
+  //   return count;
+  // }
+  switch(action.type){
+    case PLUS:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+}
+const countStore = createStore(countModifier); //expected the reducer to be a function 
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+countStore.subscribe(onChange);
+// //dispatch :: sending message to countModifier
+// countStore.dispatch({ type: "PLUS" });
+// countStore.dispatch({ type: "MINUS" });
+
+const handlePlus = () => {
+  countStore.dispatch({ type: PLUS});
+}
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS});
+}
+plus.addEventListener("click", handlePlus); //handlePlus() :: click 후 호출하는 것이기 때문에 ()를 붙이지 않는다
+minus.addEventListener("click", handleMinus); 
